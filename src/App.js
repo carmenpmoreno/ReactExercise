2266328bb0da350f77ab85f31b22bf447d7fb4af
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-06 19:18:17 +0200

    add const to App js

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import './App.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div className="main-App">
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="title-container">
-              <h1 className="App-title">Alex Guerrero</h1>
-              <p>Lunes 26 de junio de 2017</p>
-            </div>
-          </header>
-          <main>
-            <p className="paragraph">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos delectus itaque eum quae voluptatem laboriosam
-              nesciunt, accusantium dolorum ipsam neque omnis magnam totam
-              doloribus facilis natus quidem, architecto aut hic.
-            </p>
-          </main>
-          <footer className="footer">
-            <p>Leer más...</p>
-            <span>
-              37
-              <i class="fas fa-heart" />
-            </span>
-          </footer>
-        </div>
+  // NOTA: para hacer las contantes, VAMOS DE PEQUEÑO A GRANDE. Tener en cuenta que primero ponemos las que engloban los trozos de código más pequeño y a continuación las de código que engloba el anterior.
+    const headerRoot = (
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="title-container">
+        <h1 className="App-title">Alex Guerrero</h1>
+        <p>Lunes 26 de junio de 2017</p>
       </div>
+    </header>
     );
+    const mainRoot = (
+      <main>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dignissimos delectus itaque eum quae voluptatem laboriosam
+            nesciunt, accusantium dolorum ipsam neque omnis magnam totam
+            doloribus facilis natus quidem, architecto aut hic.
+          </p>
+        </main>
+    );
+    const footerRoot = (
+      <footer className="footer">
+      <p>Leer más...</p>
+      <span>
+        37
+        <i class="fas fa-heart" />
+      </span>
+    </footer>
+    );
+    
+    const pageRoot = (<div className="App">{headerRoot}{mainRoot}{footerRoot}</div>);
+
+    const appRoot = (
+      <div className="main-App">
+      {pageRoot}
+    </div>
+    );
+    return appRoot;
   }
 }
 
