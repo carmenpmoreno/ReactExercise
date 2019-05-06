6caea52cf219ea8b0ee5ce7ae25d488b752425db
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-06 18:13:16 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./images/logo-ejercicio1.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Alex Guerrero</h1>
+          <p>Lunes 26 de junio de 2017</p>
+        </header>
+        <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos delectus itaque eum quae voluptatem laboriosam nesciunt, accusantium dolorum ipsam neque omnis magnam totam doloribus facilis natus quidem, architecto aut hic.</p>
+        </main>
+        <footer>
+          <p>Leer más...</p>
+          <span>37<i class="fas fa-heart"></i></span>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
