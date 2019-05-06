360e9db085bc634ff84ca205a2d1dd9839666423
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-06 18:39:03 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { Component } from "react";
-import logo from "./images/logo-ejercicio1.png";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './images/logo-ejercicio1.png';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Alex Guerrero</h1>
-          <p>Lunes 26 de junio de 2017</p>
-        </header>
-        <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos delectus itaque eum quae voluptatem laboriosam nesciunt, accusantium dolorum ipsam neque omnis magnam totam doloribus facilis natus quidem, architecto aut hic.</p>
-        </main>
-        <footer>
-          <p>Leer más...</p>
-          <span>37<i class="fas fa-heart"></i></span>
-        </footer>
+      <div className="main-App">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="title-container">
+              <h1 className="App-title">Alex Guerrero</h1>
+              <p>Lunes 26 de junio de 2017</p>
+            </div>
+          </header>
+          <main>
+            <p className="paragraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos delectus itaque eum quae voluptatem laboriosam
+              nesciunt, accusantium dolorum ipsam neque omnis magnam totam
+              doloribus facilis natus quidem, architecto aut hic.
+            </p>
+          </main>
+          <footer className="footer">
+            <p>Leer más...</p>
+            <span>
+              37
+              <i class="fas fa-heart" />
+            </span>
+          </footer>
+        </div>
       </div>
     );
   }
