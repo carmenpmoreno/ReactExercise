30eee99875e4d512c25ce6ab9372eb00b4f7dbea
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-07 18:54:58 +0200

    34ejercicios01-02

---
+++ src/MediaCard.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './images/logo-ejercicio1.png';
+import './App.scss';
+
+//class App extends Component { ejercicio01
+class MediaCard extends React.Component {
+  render() {
+    // NOTA: para hacer las contantes, VAMOS DE PEQUEÑO A GRANDE. Tener en cuenta que primero ponemos las que engloban los trozos de código más pequeño y a continuación las de código que engloba el anterior.
+    console.log(this.props);
+    const headerRoot = (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="title-container">
+          <h1 className="App-title">Soy {this.props.name}</h1>
+          <p>{this.props.date}</p>
+        </div>
+      </header>
+    );
+    const mainRoot = (
+      <main>
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          delectus itaque eum quae voluptatem laboriosam nesciunt, accusantium
+          dolorum ipsam neque omnis magnam totam doloribus facilis natus quidem,
+          architecto aut hic.
+        </p>
+      </main>
+    );
+    const footerRoot = (
+      <footer className="footer">
+        <p>Leer más...</p>
+        <span>
+          37
+          <i className="fas fa-heart" />
+        </span>
+      </footer>
+    );
+
+    const pageRoot = (
+      <div className="App">
+        {headerRoot}
+        {mainRoot}
+        {footerRoot}
+      </div>
+    );
+
+    const appRoot = <div className="main-App">{pageRoot}</div>;
+    return appRoot;
+  }
+}
+
+//export default App;
+export default MediaCard;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import MediaCard from './MediaCard';
 import * as serviceWorker from './serviceWorker';
+import logo from './images/logo-ejercicio1.png'; //hay que importar la imagen desde mediacard para que la encuentre
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <MediaCard
+    name="Carmen"
+    date="12/02/1990"
+    image="CCC"
+    description="fffff"
+    likes="GGG"
+    heart="ffff"
+  />,
+  document.getElementById('root')
+); //lo que hay despues de MediaCard son los props que se pasan a MediaCard.js, en MediaCard.js no le pasamos ningun parámetro, react hace que estos parámetros se pasen solos
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
