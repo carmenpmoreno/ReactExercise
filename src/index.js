15f1f62cf9f630b0b00ce4993d25802b863e8ef2
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-08 19:09:12 +0200

    start exercise

--- src/index.js
+++ src/index.js
@@ -3,71 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import './App.scss';
+import Item from './Item';
 
-class Item extends React.Component {
-  render() {
-    return (
-      <div className="item">
-        <h5 className="quantity">{ this.props.quantity }</h5>
-        <div>
-          <h5>{ this.props.name }</h5>
-          <h6 className="text-muted">{ this.props.description }</h6>
-        </div>
-        <div className="badge badge-info">{ this.props.category }</div>
-        <h5 className="price">{ this.props.price }€</h5>
-      </div>
-    );
-  }
-}
+
 
 // Bootstrap classes: badge, badge-info, text-muted
 
 class ItemList extends React.Component {
   render() {
+    const items = [
+      {
+        name: 'Cereales con chocolate',
+        description: 'Cereales rellenos de chocolate',
+        quantity: 2,
+        category: 'Cereales',
+        price: 5,
+      },
+      {
+        name: 'Hamburguesa con queso',
+        description: 'Hamburguesa rica y saludable',
+        quantity: 1,
+        category: 'Fast-food',
+        price: 15,
+      },
+      {
+        name: 'Agua mineral',
+        description: 'Agua de un charco del Himalaya',
+        quantity: 2,
+        category: 'Bebida',
+        price: 5,
+      },
+    ];
+
     return (
       <ul className="item-list">
-        <li>
-          <Item 
-            name="Cereales con chocolate" 
-            description="Cereales rellenos de chocolate" 
-            quantity={2}
-            category="Cereales" 
-            price={5} 
-          />
-        </li>
-        <li>
-          <Item 
-            name="Hamburguesa con queso" 
-            description="Hamburguesa rica y saludable" 
-            quantity={1}
-            category="Fast-food" 
-            price={15}
-          />
-        </li>
-        <li>
-          <Item 
-            name="Agua mineral" 
-            description="Agua de un charco del Himalaya" 
-            quantity={2}
-            category="Bebida" 
-            price={5}
-          />
-        </li>
-        <li>
-          <Item 
-            name="Pollo con patatas" 
-            description="Pollo y patatas" 
-            quantity={2}
-            category="Pollo" 
-            price={5} 
-          />
-        </li>
+        
+        const items2 = items.map(element=><li>element</li>);
       </ul>
     );
   }
 }
 
 ReactDOM.render(
-  <ItemList />,
+  <ItemList items={items2}/>,
   document.getElementById('root')
 );
